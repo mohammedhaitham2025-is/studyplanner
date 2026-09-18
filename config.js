b487d5e7c0b7a609971cc738365f87aaceabedaa
mohammedhaitham2025-is <mohammed.haitham2025@gmail.com>
2026-09-18 22:19:04 +0300

    Update config.js

--- config.js
+++ config.js
@@ -9,6 +9,6 @@
 // Leave them empty and the planner still works, but only on this device.
 
 window.RECALL_CONFIG = {
-  SUPABASE_URL: "https://qibieiapixgsmwdengmk.supabase.co/rest/v1/",
+  SUPABASE_URL: "https://qibieiapixgsmwdengmk.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFpYmllaWFwaXhnc213ZGVuZ21rIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODk3NTEwMTcsImV4cCI6MjEwNTMyNzAxN30.908-nEd2V4rn1LRarY0bZS9PxiJiOzQbgBZOVljTQjs"
 };
